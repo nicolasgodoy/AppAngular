3e709ce84aff056f8166590f30482fcba9b1e048
nicolasgodoy <nicolasgodoy540@gmail.com>
2024-12-22 14:48:05 -0300

    eliminando controller

--- AppAngular/Controllers/EnvioEmailController.cs
+++ AppAngular/Controllers/EnvioEmailController.cs
@@ -7,34 +7,6 @@ namespace AppAngular.Controllers
     [Route("api/[controller]")]
     public class EnvioEmailController : Controller
     {
-
-        private readonly IAspNetUserService _aspNetUserService;
-
-        public EnvioEmailController(IAspNetUserService aspNetUserService) 
-        {
-            _aspNetUserService = aspNetUserService;
-        }
-
-        [HttpPost("sendEmail")]
-        public async Task<IActionResult> SendEmail(string email, string token, string subject, string message)
-        {
-            // Verificación de los parámetros
-            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(token))
-            {
-                return BadRequest("Email o contraseña no proporcionados.");
-            }
-
-            var callbackUrl = $"https://localhost:7057/api/EnvioEmailcontroller/confirmEmail?email={email}&token={token}";
-            // Preparar el cuerpo del correo
-            var body = $@"
-                 <a href=""{callbackUrl}"" 
-                    style=""padding: 10px; background-color: #007bff; color: white; text-decoration: none; border-radius: 5px;"">
-                    Confirmar Email
-                 </a>";
-
-            await _aspNetUserService.SendEmailAsync(email, token, subject, message, body);
-
-            return Ok("Correo enviado exitosamente");
-        }
+        // usar para otro posible controllador
     }
 }
